2a08dcd8a758d5fbb4ed6d639cc3ffab2c7b70e0
cdospinav16 <cdospinav16@gmail.com>
2021-11-17 16:56:04 -0500

    Creacion del archivo setting para produccion

---
+++ auth_backend/settings_prod.py
@@ -0,0 +1,151 @@
+from pathlib    import Path
+from datetime   import timedelta
+import django_heroku
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-6krs(7srf(a=s9i42&&l8v6e5-lokwuh1mzge$cu05lymqpsd5'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = ['localhost', 'ec2-18-214-140-149.compute-1.amazonaws.com',]
+
+""" CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost",
+]
+CORS_ORIGIN_ALLOW_ALL = True """
+
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'auth_app',
+    'rest_framework',
+]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_TIMELIFE'     : timedelta(minutes=55),
+    'REFRESH_TOKEN_TIMELIFE'    : timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS'     : False,
+    'BLACKLIST_AFTER_ROTATION'  : False,
+    'UPDATE_LAST_LOGIN'         : False,
+    'ALGORITHM'                 : 'HS256',
+    'USER_ID_FIEL'              : 'id',
+    'USER_ID_CLAIM'             : 'user_id',
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+AUTH_USER_MODEL = 'auth_app.User'
+
+ROOT_URLCONF = 'auth_backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'auth_backend.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE'    : 'django.db.backends.postgresql_psycopg2',
+        'NAME'      : 'dctm1dnfj79of9',
+        'USER'      : 'sxfwwnlimtreaz',
+        'PASSWORD'  : '10eed63b31f123c6edf6d15aa04a1d887e874cb59927c89dc5e999cb4ba60fa3',
+        'HOST'      : 'ec2-18-214-140-149.compute-1.amazonaws.com',
+        'PORT'      : '5432'
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
