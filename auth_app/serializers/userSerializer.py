1ed9cc0cc6cb57358376bd5bd2a49302b401374e
cdospinav16 <cdospinav16@gmail.com>
2021-11-17 16:53:55 -0500

    Se añadio la vista del verifyToken

---
+++ auth_app/serializers/userSerializer.py
@@ -0,0 +1,18 @@
+from rest_framework                         import serializers
+from auth_app.models.user                   import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'nombre', 'email', 'rol_jardinero']
+
+    def to_representation(self, obj):
+        user    = User.objects.get(id=obj.id)
+        return {
+            'id'      : user.id,
+            'username': user.username,
+            'nombre'  : user.nombre,
+            'email'   : user.email,
+            'rol_jardinero'     : user.rol_jardinero,
+        }
