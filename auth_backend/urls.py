17c710ab6b120d9297d4cf17f6b2a4e9e6d58fc8
cdospinav16 <cdospinav16@gmail.com>
2021-11-22 16:13:48 -0500

    Configuracion de Swagger

--- auth_backend/urls.py
+++ auth_backend/urls.py
@@ -3,10 +3,8 @@ from django.urls    import path, re_path
 from auth_app       import views
 from rest_framework_simplejwt.views   import (TokenObtainPairView, TokenRefreshView)
 from rest_framework import permissions
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 
 schema_view = get_schema_view(
